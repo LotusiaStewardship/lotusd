d3ceb56e6c24e004b88eed459af34184dc314341
maff1989 <matthew@urgero.net>
2025-10-29 10:17:06 -0700

    update gitian grab-packages to ignore Reinstallation errors

--- contrib/gitian-builder/target-bin/grab-packages.sh
+++ contrib/gitian-builder/target-bin/grab-packages.sh
@@ -9,5 +9,5 @@ cd /var/cache/apt/archives
 # make sure all packages with installed versions are downloaded
 # (except for held packages, which may not be available for download)
 dpkg-query -W -f '${Status}\t${Package}=${Version}\n' | grep -v ^hold | cut -f2- | xargs -n 50 apt-get install -q --reinstall -y -d > /tmp/download.log
-grep "cannot be downloaded" /tmp/download.log && { echo Could not download some packages, please run gbuild --upgrade 1>&2 ; exit 1 ; }
+grep -v "Reinstallation" /tmp/download.log | grep "cannot be downloaded" && { echo Could not download some packages, please run gbuild --upgrade 1>&2 ; exit 1 ; }
 sha256sum *.deb | sort --key 2
